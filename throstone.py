600f22be4e3f3444744eae51e084b41719d0fc1e
daunt3d <134453895+theunblker@users.noreply.github.com>
2023-11-29 17:57:04 -0700

    Fixed Source: Full Script Loop
    Works now, maybe..

--- throstone.py
+++ throstone.py
@@ -4,11 +4,13 @@ import socket
 # utilizes GNU General Public License (GPL)
 # DO NOT UTILIZE THROSTONE FOR ILLEGAL ACTIVITIES! by the Developers at Reonized LLC
 
+print("ThroStone by Reonized.\n")
+    ip = input("Enter Valid IP Address: ")
+
+
 while True:
     # create the socket object and wait for the user to enter the destination IP and port number
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("ThroStone by Reonized.\n")
-    ip = input("Enter Valid IP Address: ")
     port = int(input("IP Port: "))
     s.connect((ip, port))
     s.sendall(b"data")
